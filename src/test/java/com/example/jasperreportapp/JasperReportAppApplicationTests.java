9bc5e9a6b900ba126bc814b9ab99255610242f88
Rodolfo <rodolinux@gmail.com>
2025-06-07 11:48:19 -0300

    conexion H2 exitosa

--- src/test/java/com/example/jasperreportapp/JasperReportAppApplicationTests.java
+++ src/test/java/com/example/jasperreportapp/JasperReportAppApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.jasperreportapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class JasperReportAppApplicationTests {
+//@SpringBootTest
+//class JasperReportAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
